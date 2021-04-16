192b468001f0d59beec1deef128a516c1681ce6e
Alex Arslan <ararslan@comcast.net>
2021-04-16 13:03:02 -0700

    YAS-ify docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,24 +3,16 @@ using Documenter
 
 DocMeta.setdocmeta!(StandardizedPredictors, :DocTestSetup, :(using StandardizedPredictors); recursive=true)
 
-makedocs(;
-    modules=[StandardizedPredictors],
-    authors="Beacon Biosignals, Inc.",
-    repo="https://github.com/beacon-biosignals/StandardizedPredictors.jl/blob/{commit}{path}#{line}",
-    sitename="StandardizedPredictors.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://beacon-biosignals.github.io/StandardizedPredictors.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "API" => "api.md",
-    ],
-)
+makedocs(modules=[StandardizedPredictors],
+         authors="Beacon Biosignals, Inc.",
+         repo="https://github.com/beacon-biosignals/StandardizedPredictors.jl/blob/{commit}{path}#{line}",
+         sitename="StandardizedPredictors.jl",
+         format=Documenter.HTML(prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://beacon-biosignals.github.io/StandardizedPredictors.jl",
+                                assets=String[]),
+         pages=["Home" => "index.md",
+                "API" => "api.md"])
 
-deploydocs(;
-    repo="github.com/beacon-biosignals/StandardizedPredictors.jl",
-    devbranch="main",
-    push_preview=true
-)
+deploydocs(repo="github.com/beacon-biosignals/StandardizedPredictors.jl",
+           devbranch="main",
+           push_preview=true)
