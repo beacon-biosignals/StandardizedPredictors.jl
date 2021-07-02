f20d9cb9bfcc0d9f326936c12ab7bd08b4771c9f
Phillip Alday <phillip.alday@beacon.bio>
2021-07-02 11:01:47 -0500

    the other branch of convert errors

--- test/centering.jl
+++ test/centering.jl
@@ -75,6 +75,7 @@
             center!(mean, copy(x)) == center(mean, x)
             @test_throws ArgumentError center!(mean, [1, 2])
             @test_throws ArgumentError center!([1, 2])
+            @test_throws MethodError center!(x -> 1, ["a","b"])
         end
     end
 
